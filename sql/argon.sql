7853642aff8cada0a428834a1720bb82bfd7a7e1
geospiza <geospiza@protonmail.com>
2020-12-12 19:18:44 -0800

    Fix constraints when dropping cashshoppurchases table

--- sql/argon.sql
+++ sql/argon.sql
@@ -16,33 +16,34 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
- DROP TABLE IF EXISTS `infractions`;
-  DROP TABLE IF EXISTS `macbans`;
- DROP TABLE IF EXISTS `bans`;
-  DROP TABLE IF EXISTS `wishlists`;
-  DROP TABLE IF EXISTS `skillmacros`;
-  DROP TABLE IF EXISTS `skills`;
-  DROP TABLE IF EXISTS `parties`;
-  DROP TABLE IF EXISTS `minigamescores`;
-  DROP TABLE IF EXISTS `mapmemory`;
-  DROP TABLE IF EXISTS `keymaps`;
-    DROP TABLE IF EXISTS `guildmembers`;
-      DROP TABLE IF EXISTS `guildbbsreplies`;
-    DROP TABLE IF EXISTS `guildbbstopics`;
-  DROP TABLE IF EXISTS `guilds`;
-  DROP TABLE IF EXISTS `famelog`;
-  DROP TABLE IF EXISTS `cooldowns`;
-  DROP TABLE IF EXISTS `buddyentries`;
-   DROP TABLE IF EXISTS `questmobprogress`;
-  DROP TABLE IF EXISTS `queststatuses`;
-   DROP TABLE IF EXISTS `cashshoppurchases`;
-   DROP TABLE IF EXISTS `inventoryrings`;
-    DROP TABLE IF EXISTS `petignoreitems`;
-   DROP TABLE IF EXISTS `inventorypets`;
-   DROP TABLE IF EXISTS `inventorymounts`;
-   DROP TABLE IF EXISTS `inventoryequipment`;
-  DROP TABLE IF EXISTS `inventoryitems`;
- DROP TABLE IF EXISTS `characters`;
+DROP TABLE IF EXISTS `infractions`;
+DROP TABLE IF EXISTS `macbans`;
+DROP TABLE IF EXISTS `bans`;
+DROP TABLE IF EXISTS `wishlists`;
+DROP TABLE IF EXISTS `skillmacros`;
+DROP TABLE IF EXISTS `skills`;
+DROP TABLE IF EXISTS `parties`;
+DROP TABLE IF EXISTS `minigamescores`;
+DROP TABLE IF EXISTS `mapmemory`;
+DROP TABLE IF EXISTS `keymaps`;
+DROP TABLE IF EXISTS `guildmembers`;
+DROP TABLE IF EXISTS `guildbbsreplies`;
+DROP TABLE IF EXISTS `guildbbstopics`;
+DROP TABLE IF EXISTS `guilds`;
+DROP TABLE IF EXISTS `famelog`;
+DROP TABLE IF EXISTS `cooldowns`;
+DROP TABLE IF EXISTS `buddyentries`;
+DROP TABLE IF EXISTS `questmobprogress`;
+DROP TABLE IF EXISTS `queststatuses`;
+DROP TABLE IF EXISTS `cashitemgiftnotes`;
+DROP TABLE IF EXISTS `cashshoppurchases`;
+DROP TABLE IF EXISTS `inventoryrings`;
+DROP TABLE IF EXISTS `petignoreitems`;
+DROP TABLE IF EXISTS `inventorypets`;
+DROP TABLE IF EXISTS `inventorymounts`;
+DROP TABLE IF EXISTS `inventoryequipment`;
+DROP TABLE IF EXISTS `inventoryitems`;
+DROP TABLE IF EXISTS `characters`;
 DROP TABLE IF EXISTS `accounts`;
 
 CREATE TABLE `accounts` (
